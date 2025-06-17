d3de558892db106bd7d3cf12c84afb7187e99fb4
simone-poli <polismn97@gmail.com>
2025-06-17 14:54:59 +0200

    add title, body, js

---
+++ assets/jss/main.js
@@ -0,0 +1,35 @@
+    const rowEl = document.querySelector(".row")
+    console.log(rowEl)
+    
+    const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
+    
+    
+    
+    
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+
+    data.forEach((image) => {
+        const {title, url} = image;
+
+
+        const markup = `
+        <div class="col-sm-1 col-md-2 col-3">
+            <div class="card">
+                <div class="image">
+                    <img src="${url}" alt="${title}">
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste blanditiis dolore aliquid soluta nam rem. </p>
+                </div>
+            </div>
+        </div>
+        `
+        
+    rowEl.insertAdjacentElement('beforeend', markup)
+    });
+    })
+    
+
