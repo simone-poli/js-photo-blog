d65f6097db4ac62c25f1f421d1601a97159efc69
simone-poli <polismn97@gmail.com>
2025-06-18 15:02:06 +0200

    add modal

--- assets/jss/main.js
+++ assets/jss/main.js
@@ -1,9 +1,12 @@
     const rowEl = document.querySelector(".row")
-    console.log(rowEl)
-    
+    const modalEl = document.getElementById("modal")
+    const modalImg = document.querySelector(".modalImg")
+
     const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
     
-    
+
+
+
     
     
     fetch(endpoint)
@@ -14,15 +17,15 @@
     data.forEach((image) => {
         const {title, url} = image
 
-
+        
         const markup = `
         <div class="col-sm-1 col-md-2 col-3">
             <div class="card">
                 <div class="image">
-                    <img src="${url}" alt="${title}">
+                   <img class="imageThumb" src="${url}" alt="${title}"> 
                 </div>
                 <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste blanditiis dolore aliquid soluta nam rem. </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste blanditiis dolore aliquid soluta nam rem. </p>
                 </div>
             </div>
         </div>
@@ -30,6 +33,17 @@
         
     rowEl.insertAdjacentHTML('beforeend', markup)
     });
+    
+    const allImages = document.querySelectorAll('.imageThumb')
+
+    allImages.forEach(img =>{
+        img.addEventListener('click', () =>{
+            modalEl.style.display = 'block'
+            modalImg.src = img.src;
+            console.log(img.src)
+        })
+        
+    })
     })
     
 
